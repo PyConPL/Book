1cc425b8210430e743bb9eba601b7ed0bca90b8c
xando <sebastian.pawlus@gmail.com>
2014-10-02 09:19:14 +0200

    wołanie pandoc poprawione

--- 2014/make_file.sh
+++ 2014/make_file.sh
@@ -1,2 +1,2 @@
-cp presentations/Optimizing_API_against_latency/cisco.png ./
-find . -name "text.md" -print | xargs pandoc -o text.pdf
+cp presentations/Optimizing_API_against_latency/cisco.png ./ 
+find . -name "text.md" -print | xargs pandoc -o text.pdf --template template.tex -s --toc --toc-depth=1
