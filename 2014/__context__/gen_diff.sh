68a6c2642f5f3cb5babb892f2d684fb1ecca026f
Piotr Kasprzyk <ciri@ciri.pl>
2014-10-09 11:51:55 +0200

    Enforce LABEL

--- 2014/__context__/gen_diff.sh
+++ 2014/__context__/gen_diff.sh
@@ -3,7 +3,8 @@ NAMES=`cd expected;find * -type f -name \*.tex`
 for i in $NAMES
 do
     FILE=`basename $i .tex`
+    LABEL=`basename $i`
     DST=src/$FILE.diff
-    diff -ru build/$i expected/$i > $DST
+    diff -ru -L $LABEL build/$i expected/$i > $DST
     echo $DST done.
 done
