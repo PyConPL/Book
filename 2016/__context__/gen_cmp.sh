7ed2c8652623abe1aa4b6bdef9c40f7b67688a12
Piotr Kasprzyk <ciri@ciri.pl>
2016-09-27 14:47:06 +0200

    Add gen_cmp.sh

---
+++ 2016/__context__/gen_cmp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+EXP_DIR=expected
+if [ ! -e $EXP_DIR ]
+then
+    echo Creating the directory '"'$EXP_DIR'"'.
+    mkdir $EXP_DIR
+fi
+
+if [ -d $EXP_DIR ]
+then
+    (cd build; tar cf - `find . -name \*.tex`) | (cd expected; tar -xvf -)
+else
+    echo Wrong type of '"'$EXP_DIR'"' - it should be a directory:
+    ls -ld $EXP_DIR
+fi
